1c820b20748c63800402960ae8f3c84bf53c5f8c
linye <linye102@163.com>
2022-05-15 16:12:00 +0800

    init

---
+++ src/test/java/com/lincoln/websocket/SpringbootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincoln.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
